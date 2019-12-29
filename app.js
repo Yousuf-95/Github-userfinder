1002bfcd21714ca3c5e08a217678cf73073e123f
Yousuf <yf.yousuf95@gmail.com>
2019-12-29 11:42:44 +0530

    Initial UI design

---
+++ app.js
@@ -0,0 +1,13 @@
+const searchUser = document.getElementById('searchUser');
+
+
+//Add event listener to input.
+searchUser.addEventListener('keyup', (event) => {
+
+    //Get input text
+    const userText = event.target.value;
+    console.log(userText);
+    if(userText !== ''){
+        
+    }
+})
